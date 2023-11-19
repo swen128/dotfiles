1495760a348935b91f88fbb92cb9e53504157564
PND <fujjisaaan@gmail.com>
2023-11-19 16:52:27 +0900

    Install the Copilot plugin to neovim.

--- home/dot_config/nvim/lua/init-native.lua
+++ home/dot_config/nvim/lua/init-native.lua
@@ -167,6 +167,15 @@ require('lazy').setup({
     },
     build = ':TSUpdate',
   },
+
+  {
+    'github/copilot.vim',
+    lazy = false,
+    config = function()
+      vim.g.copilot_no_tab_map = true
+      vim.api.nvim_set_keymap("i", "<C-i>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+    end
+  },
 }, {})
 
 -- [[ Setting options ]]
