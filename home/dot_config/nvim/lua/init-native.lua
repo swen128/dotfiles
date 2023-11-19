58c7b122d2764f478b4688edad413bf8c5c93ab0
PND <fujjisaaan@gmail.com>
2023-11-19 17:52:19 +0900

    Remove unnecessary neovim shortcuts.

--- home/dot_config/nvim/lua/init-native.lua
+++ home/dot_config/nvim/lua/init-native.lua
@@ -97,11 +97,6 @@ require('lazy').setup({
         topdelete = { text = '‾' },
         changedelete = { text = '~' },
       },
-      on_attach = function(bufnr)
-        vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk, { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
-        vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
-        vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[P]review [H]unk' })
-      end,
     },
   },
 
