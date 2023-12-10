a0752e7d26c9b4fde8c2b093b5ae8732d613bb5e
PND <fujjisaaan@gmail.com>
2023-12-10 12:23:07 +0900

    Enable rust-analyzer in neovim.

--- home/dot_config/nvim/lua/init-native.lua
+++ home/dot_config/nvim/lua/init-native.lua
@@ -400,7 +400,7 @@ local servers = {
   -- clangd = {},
   -- gopls = {},
   -- pyright = {},
-  -- rust_analyzer = {},
+  rust_analyzer = {},
   tsserver = {},
   -- html = { filetypes = { 'html', 'twig', 'hbs'} },
 
