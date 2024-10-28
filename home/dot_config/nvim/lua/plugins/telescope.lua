76caa8f987d8c2d0fbafb04a7c3535cbd3027f1a
PND <fujjisaaan@gmail.com>
2024-10-28 22:11:46 +0900

    Tweak telescope-neovim UI.

--- home/dot_config/nvim/lua/plugins/telescope.lua
+++ home/dot_config/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,12 @@
 return {
   "nvim-telescope/telescope.nvim",
+  opts = {
+    defaults = {
+      path_display = { "smart" },
+      dynamic_preview_title = true,
+      layout_strategy = "vertical",
+    },
+  },
   keys = {
     -- Keymap for telescope-sg, which integrates ast-grep into telescope.
     { "<leader>sa", "<cmd>Telescope ast_grep<cr>", desc = "AST grep" },
