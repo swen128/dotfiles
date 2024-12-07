87683804a18c5d148bb5adad2b10ffab9d577d63
PND <fujjisaaan@gmail.com>
2024-12-07 16:26:49 +0900

    Add language supports for LazyVim.

--- home/dot_config/nvim/lua/config/lazy.lua
+++ home/dot_config/nvim/lua/config/lazy.lua
@@ -20,6 +20,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.lang.terraform" },
+    { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.vscode" },
 
     { import = "plugins" },
