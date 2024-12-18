7f3e14a44f9b28bf89c8c7640bad6a72a73fd0f7
PND <fujjisaaan@gmail.com>
2024-12-18 20:51:15 +0900

    Replace telescope.nvim with fzf-lua.

---
+++ home/dot_config/nvim/lua/plugins/fzf.lua
@@ -0,0 +1,14 @@
+return {
+  "ibhagwan/fzf-lua",
+  opts = {
+    defaults = {
+      formatter = "path.filename_first",
+      winopts = {
+        preview = {
+          layout = "flex",
+          flip_columns = 150,
+        },
+      },
+    },
+  },
+}
--- home/dot_config/nvim/lua/plugins/telescope-sg.lua
+++
@@ -1,3 +0,0 @@
-return {
-  "Marskey/telescope-sg",
-}
