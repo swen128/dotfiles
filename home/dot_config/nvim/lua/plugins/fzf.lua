6c0a293f5c29df78b9fc9a37eb3bb2822f4de6c6
PND <fujjisaaan@gmail.com>
2024-12-19 22:40:18 +0900

    Integrate zoxide with fzf-lua.

--- home/dot_config/nvim/lua/plugins/fzf.lua
+++ home/dot_config/nvim/lua/plugins/fzf.lua
@@ -1,7 +1,7 @@
 return {
   "ibhagwan/fzf-lua",
-  opts = {
-    defaults = {
+  config = function()
+    require("fzf-lua").setup({
       formatter = "path.filename_first",
       winopts = {
         preview = {
@@ -9,6 +9,22 @@ return {
           flip_columns = 150,
         },
       },
-    },
-  },
+    })
+
+    -- zoxide integration
+    vim.keymap.set("n", "<leader>z", function()
+      require("fzf-lua").fzf_live("zoxide query -l", {
+        prompt = "zoxide> ",
+        actions = {
+          ["default"] = function(selected)
+            local dir = selected[1] or ""
+            if dir ~= "" then
+              vim.cmd("cd " .. vim.fn.fnameescape(dir))
+              print("Changed directory to: " .. dir)
+            end
+          end,
+        },
+      })
+    end, { desc = "Zoxide Jump" })
+  end,
 }
