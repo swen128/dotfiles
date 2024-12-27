0744343338043bf9fcb516c49a1185d0285fc2c1
PND <fujjisaaan@gmail.com>
2024-12-27 22:18:31 +0900

    Enable Supertab for completion in nvim.

---
+++ home/dot_config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,20 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    keymap = {
+      preset = "super-tab",
+    },
+    completion = {
+      list = {
+        selection = "manual",
+      },
+    },
+    enabled = function()
+      local disabled = false
+      disabled = disabled or (vim.bo.buftype == "prompt")
+      disabled = disabled or (vim.fn.reg_recording() ~= "")
+      disabled = disabled or (vim.fn.reg_executing() ~= "")
+      return not disabled
+    end,
+  },
+}
