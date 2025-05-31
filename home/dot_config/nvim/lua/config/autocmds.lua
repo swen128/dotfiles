fdeb2c417d096b560496909c503ef515030b3b3b
PND <fujjisaaan@gmail.com>
2025-05-31 22:48:40 +0900

    Disable the spell checker in nvim.

--- home/dot_config/nvim/lua/config/autocmds.lua
+++ home/dot_config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- The default spell checker is awful, so undo the following autocmd:
+-- https://github.com/LazyVim/LazyVim/blob/25abbf546d564dc484cf903804661ba12de45507/lua/lazyvim/config/autocmds.lua#L97-L105
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.spell = false
+  end,
+})
