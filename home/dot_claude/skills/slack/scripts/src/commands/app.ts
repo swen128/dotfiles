983318b1fd6467d23279c15f4a17de9229bc68b8
PND <fujjisaaan@gmail.com>
2026-05-15 22:05:44 +0900

    refactor: remove slack skill

--- home/dot_claude/skills/slack/scripts/src/commands/app.ts
+++
@@ -1,9 +0,0 @@
-import { buildManifest } from "../slack/manifest.ts";
-
-export interface AppManifestArgs {
-  port: number;
-}
-
-export function appManifestCommand(args: AppManifestArgs): void {
-  process.stdout.write(buildManifest(args.port));
-}
